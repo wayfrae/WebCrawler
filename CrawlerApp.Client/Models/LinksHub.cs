0962c8d92e207c8e46dab03a72a7cca202e3cd2f
Zach Frazier <frazier.zach@gmail.com>
2019-04-16 19:35:00 -0600

    **WIP** implementing API DataStore

--- CrawlerApp.Client/Models/LinksHub.cs
+++ CrawlerApp.Client/Models/LinksHub.cs
@@ -13,36 +13,7 @@ namespace CrawlerApp.Client.Models
     {
         DataStorageMySql db;
         int previousCount = 0;
-
-        public override Task OnConnectedAsync()
-        {
-            db = new DataStorageMySql(new List<DataStore.Link>());
-            Start();
-            return base.OnConnectedAsync();
-        }
-
-        string connectionString = "server=school-projects.mysql.database.azure.com;uid=wayfrae@school-projects;pwd=Password1;database=webcrawler";
-
-        void Start()
-        {
-            Timer timer = new Timer(1000);
-            timer.Elapsed += OnTimedEvent;
-            timer.Enabled = true;
-        }
-
-        private void OnTimedEvent(object sender, ElapsedEventArgs e)
-        {
-            int value = db.CountRows();
-            if (previousCount != value)
-            {
-                NotifyChange();
-            }
-            previousCount = value;
-        }
-
-        void Stop()
-        {
-        }
+              
 
         public async Task NotifyChange()
         {
